e2ad40c0860b4fb66d79ef31ce3efe8f7f85bb34
Fernando Reis Guimarães <fernandobhz@gmail.com>
2020-07-25 18:58:00 -0300

    old project migrated

---
+++ Break Down SQL Script into Batches Split go/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim f = From sql In System.IO.Directory.GetFiles("c:\sqls\", "*.sql") Order By sql
+
+        For Each s As String In f
+
+            Dim FullSQL As String = System.IO.File.ReadAllText(s)
+            FullSQL = FullSQL.Replace(vbCrLf & "go" & vbCrLf, vbCrLf & "|" & vbCrLf)
+            FullSQL = FullSQL.Replace(vbCrLf & "Go" & vbCrLf, vbCrLf & "|" & vbCrLf)
+            FullSQL = FullSQL.Replace(vbCrLf & "gO" & vbCrLf, vbCrLf & "|" & vbCrLf)
+            FullSQL = FullSQL.Replace(vbCrLf & "GO" & vbCrLf, vbCrLf & "|" & vbCrLf)
+
+            Dim batches() As String = FullSQL.Split("|")
+
+            For Each b As String In batches
+                MsgBox(b)
+            Next
+
+        Next
+
+    End Sub
+End Class
